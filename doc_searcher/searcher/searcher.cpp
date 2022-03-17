596e461316f70761da8868b348cde72e30cfe150
Jeremy俊濠 <8371745+jeremy-junhao@user.noreply.gitee.com>
2022-03-17 15:03:28 +0800

    [add]searcher模块

--- doc_searcher/searcher/searcher.cpp
+++ doc_searcher/searcher/searcher.cpp
@@ -167,4 +167,85 @@ namespace searcher{
         
         return &(it->second);
     }
+
+
+
+    // 搜索模块
+    const int lengthText = 160;
+
+    /*
+    初始化 构建指定文档索引
+    */
+    bool Searcher::Init(const string& input_path){
+       return index->Build(input_path);
+   }
+
+    /*
+    指定文本进行搜索
+    */
+    bool Searcher::Search(const string& query, string* output){
+        // 分词
+        vector<string> words;
+        index->CutWord(query, &words);
+
+        // 触发 根据分词的结果 进行倒排索引 得到相关文档
+        vector<backwardIdx> wordsResult;
+        for(string word : words){
+            boost::to_lower(word);
+            auto* backList = index->GetBackwardIdx(word);
+            if(backList == nullptr){
+                // 没有这个关键词
+                continue;
+            }
+            // 插入多个数据
+            wordsResult.insert(wordsResult.end(), backList->begin(), backList->end());
+        }
+
+        if(wordsResult.size() == 0){
+            // 没有与内容相关数据 404 Not Found
+            return false;
+        }
+
+        // 排序
+        std::sort(wordsResult.begin(), wordsResult.end(),
+                [](const backwardIdx& le, const backwardIdx& ri){
+                    return le._weight > ri._weight;
+                });
+
+        // 包装
+        Json::Value value;
+        for(const auto& backidx : wordsResult){
+            // 根据 id 查找正排索引
+            const frontIdx* doc_info = index->GetFrontIdx(backidx._docId);
+            
+            Json::Value tmp;
+            tmp["title"]    = doc_info->_title;
+            tmp["url"]      = doc_info->_url;
+            tmp["desc"]     = GetShowContent(doc_info->_content, backidx._word);
+            value.append(tmp);
+        }
+        Json::FastWriter writer;
+        *output = writer.write(value);
+        return true;
+    }
+
+    /*
+    得到关键字前后的数据 在前端页面显示的文本
+    */
+    string Searcher::GetShowContent(const string& content,const string& word){
+        size_t idx = content.find(word);
+        string ans("");
+        int pos = 0;    // 显示文本开始的位置
+        int len = lengthText;   // 截取显示文本的长度
+        if(idx == string::npos){
+            //关键字不存在
+            len = std::min(len, (int)content.size());
+        }else{
+            pos = std::max(0, (int)((int)idx-lengthText/2));
+            len = std::min((int)lengthText/2, (int)(content.size() - idx));
+        }
+        ans = content.substr(pos, len);
+        ans += "...";
+        return ans;
+    }
 }
--- doc_searcher/searcher/searcher_test.cpp
+++ doc_searcher/searcher/searcher_test.cpp
@@ -1,7 +1,7 @@
 #include "searcher.hpp"
 #include <ostream>
 
-int main() {
+int indexTest(){
     searcher::Index index;
     bool ret = index.Build("../data/tmp/raw_input.txt");
     if (!ret) {
@@ -19,5 +19,32 @@ int main() {
         std::cout << "content:" << doc_info->_content << std::endl;
         std::cout << "================================================================" << std::endl;
     }
+    return 0;
+}
+
+int searcherTest(){
+    searcher::Searcher searcher;
+    bool ret = searcher.Init("../data/tmp/raw_input.txt");
+    if(!ret){
+        std::cout<<"Searcher 初始化失败"<<std::endl;
+        return 1;
+    }
+    while(true) {
+        std::cout<< "search >" <<std::flush;
+        string query;
+        std::cin >> query;
+        if(!std::cin.good()){
+            std::cout<< "goodbye" << std::endl;
+            break;
+        }
+        string results;
+        searcher.Search(query, &results);
+        std::cout << results << std::endl;
+    }
+    return 0;
+}
+
+int main() {
+    int testResult = searcherTest();
     return 0;
 }
